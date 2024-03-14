0c066eefc80d904479185b362bdc77a00da0150f
singodiyashubham87 <singodiyashubham87@gmail.com>
2024-03-15 00:17:03 +0530

    feat: add event listeners for touchscreen devices

--- src/utils/canvas.js
+++ src/utils/canvas.js
@@ -1,7 +1,7 @@
 export function startDrawing(color, lineThickness) {
   const canvas = document.getElementById("draw");
   const ctx = canvas.getContext("2d");
-  canvas.width = window.innerWidth * 0.7;
+  canvas.width = window.innerWidth * 0.8;
   canvas.height = window.innerHeight * 0.6;
   ctx.strokeStyle = `${color}`;
   ctx.lineJoin = "round";
@@ -34,6 +34,30 @@ export function startDrawing(color, lineThickness) {
   canvas.addEventListener("mouseup", () => (isDrawing = false));
   canvas.addEventListener("mouseout", () => (isDrawing = false));
   canvas.addEventListener("mousemove", draw);
+
+  //Event listeners for touch devices
+  canvas.addEventListener("touchstart", (e) => {
+    const touch = e.touches[0]; // Get the first touch
+    lastX = touch.clientX - canvas.offsetLeft;
+    lastY = touch.clientY - canvas.offsetTop;
+    isDrawing = true;
+  });
+
+  canvas.addEventListener("touchend", () => {
+    isDrawing = false;
+  });
+
+  canvas.addEventListener("touchcancel", () => {
+    isDrawing = false;
+  });
+
+  canvas.addEventListener("touchmove", (e) => {
+    if (!isDrawing) return;
+    const touch = e.touches[0]; // Get the first touch
+    const offsetX = touch.clientX - canvas.offsetLeft;
+    const offsetY = touch.clientY - canvas.offsetTop;
+    draw({ offsetX, offsetY });
+  });
 }
 
 export function clearCanvas() {
