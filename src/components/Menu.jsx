3dfd88fb86450dfd985c7d2316410c4c5c8d6aae
Vidip Ghosh <vidip2001@gmail.com>
2024-05-11 22:39:06 +0530

    feat: Export to PDF and SVG

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,8 +2,10 @@
 import { PiPencilSimpleFill } from "react-icons/pi";
 import { FaFeatherPointed } from "react-icons/fa6";
 import { RiScreenshot2Fill } from "react-icons/ri";
+import {FaFilePdf} from 'react-icons/fa'
+import { TbFileTypeSvg } from "react-icons/tb";
 import { useState } from "react";
-import { takeSnapshot } from "../utils/canvas.js";
+import { takeSnapshot,convertToPDF,convertToSVG } from "../utils/canvas.js";
 
 const Menu = ({
   isDrawing,
@@ -15,7 +17,15 @@ const Menu = ({
   canvasRef,
 }) => {
   const [pencilWidth, setPencilWidth] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
+  const handleMouseEnter = () => {
+    setIsOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsOpen(false);
+  };
   const toggleIsDrawing = () => {
     setIsDrawing(!isDrawing);
   };
@@ -59,11 +69,53 @@ const Menu = ({
             className={`bg-[#CBCCCF] p-[0.5rem] shadow-vsm rounded-[0.5rem] cursor-pointer outline-none hover:bg-[#B7BABF] flex-[0.5] w-full h-full z-[5] absolute top-0 left-0`}
           />
         </div>
-        <RiScreenshot2Fill
-          className={`text-[2rem] md:text-[3rem] p-[0.5rem] md:p-[0.8rem] shadow-vsm rounded-[0.5rem] cursor-pointer hover:bg-[#B7BABF]`}
-          onClick={() => takeSnapshot(canvasRef.current, color)}
-          title="Snapshot"
-        />
+        <div className="relative">
+          <button
+            id="dropdownHoverButton"
+            data-dropdown-toggle="dropdownHover"
+            data-dropdown-trigger="hover"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            type="button"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            Save As
+            <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+            </svg>
+          </button>
+
+          <div
+            id="dropdownHover"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className={`absolute z-10 ${isOpen ? '' : 'hidden'} divide-y bg-[#CBCCCF] rounded-lg shadow w-44 top-[2.6rem]`}
+          >
+            <ul className="text-sm text-gray-700" aria-labelledby="dropdownHoverButton">
+              <li>
+                <RiScreenshot2Fill
+                  className={`text-[2rem] md:text-[3rem] p-[0.5rem] md:p-[0.8rem] shadow-vsm rounded-[0.5rem] cursor-pointer hover:bg-[#B7BABF]`}
+                  onClick={() => takeSnapshot(canvasRef.current, color)}
+                  title="Snapshot"
+                />
+              </li>
+              <li>
+                <FaFilePdf 
+                  className={`text-[2rem] md:text-[3rem] p-[0.5rem] md:p-[0.8rem] shadow-vsm rounded-[0.5rem] cursor-pointer hover:bg-[#B7BABF]`}
+                  onClick={()=>convertToPDF(canvasRef.current)}
+                  title="PDF"
+                />
+              </li>
+              <li>
+                <TbFileTypeSvg
+                  className={`text-[2rem] md:text-[3rem] p-[0.5rem] md:p-[0.8rem] shadow-vsm rounded-[0.5rem] cursor-pointer hover:bg-[#B7BABF]`}
+                  onClick={()=>convertToSVG(canvasRef.current)}
+                  title="SVG"
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
