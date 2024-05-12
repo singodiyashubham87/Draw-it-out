36a87113fdf618a231c8f63831075d5be95db546
unknown <saharialeora2406@gmail.com>
2024-05-12 14:28:29 +0530

    deleted chatbot files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-//import Chatbot from './Chatbot/Chatbot.jsx'; // Import the Chatbot component
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
