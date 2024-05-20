de4dcadd1f8dda64cda9e7a3ed1787d9d540a39c
Aiswarya Sini Lal <150686467+Asl2027@users.noreply.github.com>
2024-05-20 14:44:43 +0530

    Added Footer with Social Media Icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { RxCross1 } from "react-icons/rx";
 import { useEffect, useState, useRef } from "react";
-import { startDrawing, clearCanvas, handleUpdates } from "./utils/canvas";
+import { startDrawing, clearCanvas } from "./utils/canvas";
 import Menu from "./components/Menu";
 import BgColor from "./components/BgColor";
 import { rainbowColors } from "./utils/helpers";
-import Footer from "./components/Footer"; // From addicons
-import { FaRegEye, FaRegEyeSlash, FaMoon, FaSun } from "react-icons/fa"; // From main
-import Joyride from "react-joyride"; // From main
+import Footer from "./components/Footer";
+import { FaRegEye, FaRegEyeSlash, FaMoon, FaSun } from "react-icons/fa";
+import Joyride from "react-joyride";
+import { SiBuymeacoffee } from "react-icons/si";
 
-const tourSteps = [ // From main
+const tourSteps = [
   {
     target: "body",
     placement: "center",
@@ -40,93 +41,51 @@ const tourSteps = [ // From main
   },
 ];
 
-import { SiBuymeacoffee } from "react-icons/si"; // From main
-
 function App() {
   const canvasRef = useRef(null);
   const [isDrawing, setIsDrawing] = useState(true);
-  const [thickness, setThickness] = useState(10);
+  const [thickness, setThickness] = useState(4);
   const [color, setColor] = useState("#000");
   const [bgColor, setBgColor] = useState("#b7babf");
   const [darkMode, setDarkMode] = useState(null);
   const [showMenuAndBgColor, setShowMenuAndBgColor] = useState(true);
   const [steps] = useState(tourSteps);
 
-  const BUY_ME_COFFEE_LINK = "https://buymeacoffee.com/mastermickey"
+  const BUY_ME_COFFEE_LINK = "https://buymeacoffee.com/mastermickey";
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    
+
     if (canvas) {
       startDrawing(canvas, color, thickness, bgColor);
     }
-  }, []);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    
-    if (canvas) {
-      handleUpdates(canvas, color, thickness, bgColor);
-    }
-  }, [thickness, color]);
+  }, [thickness, color, bgColor]);
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
-    document.body.classList.toggle('dark')
+    document.body.classList.toggle("dark");
   };
 
   return (
     <>
-      <div className="bg-[#CBCCCF] flex flex-col min-w-full justify-center gsm:flex-row dark:bg-zinc-800 dark:bg-blend-luminosity dark:text-white transform transition duration-500 ease-in-out">
-        <Joyride steps={steps} continuous showSkipButton={true} />
-        
-        {/* Buy me a coffee element */}
-        <a href={BUY_ME_COFFEE_LINK} target="_blank" rel="noopener noreferrer" className="sm:absolute flex items-center right-10 top-4 relative ml-[90%] sm:ml-0 ">
-          <button className="flex items-center bg-transparent border-4 border-black text-black focus:outline-none bg-[#d4d5d7] transform transition duration-300 ease-in-out hover:bg-gray-400 rounded-xl p-2 dark:border-black dark:border-4 dark:bg-gray-400  dark:hover:md:bg-gray-100 hover:md:scale-110 dark:shadow-lg dark:shadow-black">
-            <SiBuymeacoffee className="text-xl mx-auto sm:mr-2 dark:text-black" /> {/* Icon */}
-            <span className="hidden sm:block text-base font-cursive dark:text-black ">Buy me a Coffee</span> {/* Text */}
-          </button>
-        </a>
-        
-        {showMenuAndBgColor && (
-          <div className="color-pallet gsm:w-[10%] w-[85%] py-7 grid grid-cols-6 vsm:grid-cols-4 gsm:grid-cols-1 gap-2 vsm:gap-4 gsm:gap-2 gsm:py-[5rem] gsm:mb-8 mx-auto">
-            <input
-              type="color"
-              name="color"
-              id="color"
-              title="Color Picker"
-              onChange={(e) => setBgColor(e.target.value)}
-              className={`cursor-pointer m-auto w-[2rem] h-[2rem] vsm:w-[3rem] vsm:h-[3rem]  rounded-[0.4rem] border-[0.2px] border-black bg-gradient-to-r from-red-700 via-yellow-600 to-green-600 `}
-            />
-            {rainbowColors?.map((val, i) => (
-              <BgColor
-                key={i}
-                color={val}
-                setBgColor={setBgColor}
-                canvas={canvasRef.current}
-              />
-            ))}
-          </div>
-        )}
+      <Joyride steps={steps} continuous showSkipButton={true} />
 
-        <div className="container w-[90%] gsm:min-h-[100dvh] flex flex-col justify-center items-center gap-[2rem] font-primary m-auto gsm:m-0 dark:text-black shadow-none">
-          <div className="flex items-center gap-14 shadow-black">
-            {showMenuAndBgColor && (
-              <Menu
-                isDrawing={isDrawing}
-                setIsDrawing={setIsDrawing}
-                thickness={thickness}
-                setThickness={setThickness}
-                color={color}
-                setColor={setColor}
-                canvasRef={canvasRef}
-                bgColor={bgColor}
-              />
-            )}
+      <div className="bg-[#d3d5d8] flex flex-col min-w-full justify-center gsm:flex-row">
+        <div className="container w-[90%] gsm:min-h-[100dvh] flex flex-col justify-center items-center gap-[2rem] font-primary m-auto gsm:m-0">
+          <div className="flex items-center gap-14">
+            <Menu
+              isDrawing={isDrawing}
+              setIsDrawing={setIsDrawing}
+              thickness={thickness}
+              setThickness={setThickness}
+              color={color}
+              setColor={setColor}
+              canvasRef={canvasRef}
+            />
 
             <div className="flex flex-row justify-center align-center space-x-10">
               <div
-                className={`clearAll bg-[#CBCCCF] p-[1rem] text-[1.5rem] rounded-[50%] shadow-black shadow-md transform transition duration-300 ease-in-out text-black hover:bg-gray-400 cursor-pointer dark:bg-slate-800 dark:text-[#ffffff] hover:md:scale-110 ${
+                className={`clearAll bg-[#CBCCCF] p-[1rem] text-[1.5rem] rounded-full shadow-black shadow-md transform transition duration-300 ease-in-out text-black hover:bg-gray-400 cursor-pointer dark:bg-slate-800 dark:text-[#ffffff] hover:md:scale-110 ${
                   !showMenuAndBgColor && "mt-10"
                 }`}
                 onClick={() => {
@@ -137,7 +96,7 @@ function App() {
               </div>
 
               <div
-                className={`darkLightModeToggle p-[1rem] text-[1.5rem] rounded-[50%] shadow-md hover:bg-gray-1000 transform transition duration-300 ease-in-out hover:md:scale-110 cursor-pointer bg-black dark:bg-amber-400 shadow-black dark:shadow-black dark:shadow-md ${
+                className={`darkLightModeToggle p-[1rem] text-[1.5rem] rounded-full shadow-md hover:bg-gray-1000 transform transition duration-300 ease-in-out hover:md:scale-110 cursor-pointer bg-black dark:bg-amber-400 shadow-black dark:shadow-black dark:shadow-md ${
                   !showMenuAndBgColor && "mt-10"
                 }`}
                 onClick={toggleDarkMode}
@@ -146,7 +105,7 @@ function App() {
               </div>
             </div>
           </div>
-          
+
           <canvas
             id="draw"
             className={`whiteboard bg-slate-950 rounded-[0.6rem] shadow-md shadow-black dark:shadow-black dark:shadow-lg ${
@@ -154,9 +113,9 @@ function App() {
             }`}
             ref={canvasRef}
           ></canvas>
-          
+
           <div
-            className="clearAll bg-[#CBCCCF] p-[1rem] text-[2rem] rounded-[50%] shadow-black shadow-vsm dark:shadow-black dark:shadow-lg hover:bg-gray-400 cursor-pointer transform transition duration-300 ease-in-out dark:bg-red-700 dark:text-[#111111]  hover:md:scale-110"
+            className="clearAll bg-[#CBCCCF] p-[1rem] text-[2rem] rounded-full shadow-black shadow-vsm dark:shadow-black dark:shadow-lg hover:bg-gray-400 cursor-pointer transform transition duration-300 ease-in-out dark:bg-red-700 dark:text-[#111111]  hover:md:scale-110"
             onClick={() => {
               clearCanvas(canvasRef.current, bgColor);
               setIsDrawing(true);
@@ -164,7 +123,7 @@ function App() {
           >
             <RxCross1 />
           </div>
-          
+
           <h1 className="text-[0.7rem] vvsm:text-[1rem] pb-4 dark:text-white ">
             Made with &#128157; by{" "}
             <a
@@ -176,7 +135,24 @@ function App() {
             !
           </h1>
         </div>
+
+        {showMenuAndBgColor && (
+          <div className="color-pallet gsm:w-[10%] w-[85%] py-7 grid grid-cols-6 vsm:grid-cols-4 gsm:grid-cols-1 gap-2 vsm:gap-4 gsm:gap-2 gsm:py-[5rem] gsm:mb-8 mx-auto">
+            <input
+              type="color"
+              name="color"
+              id="color"
+              title="Color Picker"
+              onChange={(e) => setBgColor(e.target.value)}
+              className="cursor-pointer m-auto w-[2rem] h-[2rem] vsm:w-[3rem] vsm:h-[3rem] rounded-[0.4rem] border-[0.2px] border-black bg-gradient-to-r from-red-700 via-yellow-600 to-green-600"
+            />
+            {rainbowColors?.map((val, i) => (
+              <BgColor key={i} color={val} setBgColor={setBgColor} canvas={canvasRef.current} />
+            ))}
+          </div>
+        )}
       </div>
+
       <Footer />
     </>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,30 +3,37 @@ import LinkedInIcon from '../assets/images/linkedin.jpg';
 import TwitterIcon from '../assets/images/X.png'; 
 import GitHubIcon from '../assets/images/github.png'; 
 import EmailIcon from '../assets/images/GMail.webp';
-import './style.css'; 
 
 const Footer = () => {
     return (
-        <footer>
-            <div className="footercontainer">
-                <div className="socialicons">
-                    <a href="https://www.linkedin.com/in/singodiyashubham87/"><img src={LinkedInIcon} alt="LinkedIn" style={{ color: '#1d4486', fontSize: '3em' }} /></a>
-                    <a href="https://twitter.com/_master_mickey"><img src={TwitterIcon} alt="Twitter" style={{ color: '#050505', fontSize: '3em' }} /></a>
-                    <a href="https://github.com/singodiyashubham87"><img src={GitHubIcon} alt="GitHub" style={{ color: '#000000', fontSize: '3em' }} /></a>
-                    <a href="mailto:singodiyashubham87@gmail.com"><img src={EmailIcon} alt="Email" style={{ color: '#000000', fontSize: '3em' }} /></a>
-                </div>
+        <footer className="bg-black">
+            <div className="flex justify-between items-center py-4 px-8">
                 <div className="footerNav">
-                    <ul>
-                        <li><a href="">Home</a></li>
-                        <li><a href="">News</a></li>
-                        <li><a href="">About</a></li>
-                        <li><a href="">Contact Us</a></li>
-                        <li><a href="">Our Team</a></li>
+                    <ul className="flex gap-4">
+                        <li><a href="#" className="text-white hover:text-gray-600">Home</a></li>
+                        <li><a href="#" className="text-white hover:text-gray-600">News</a></li>
+                        <li><a href="#" className="text-white hover:text-gray-600">About</a></li>
+                        <li><a href="#" className="text-white hover:text-gray-600">Contact Us</a></li>
+                        <li><a href="#" className="text-white hover:text-gray-600">Our Team</a></li>
                     </ul>
                 </div>
+                <div className="socialicons flex gap-4">
+                    <a href="https://www.linkedin.com/in/singodiyashubham87/" className="hover:bg-blue-300 p-1 rounded-full">
+                        <img src={LinkedInIcon} alt="LinkedIn" className="rounded-full w-8 h-8 hover:bg-blue-500" />
+                    </a>
+                    <a href="https://twitter.com/_master_mickey" className="hover:bg-blue-300 p-1 rounded-full">
+                        <img src={TwitterIcon} alt="Twitter" className="rounded-full w-8 h-8 hover:bg-blue-500" />
+                    </a>
+                    <a href="https://github.com/singodiyashubham87" className="hover:bg-blue-300 p-1 rounded-full">
+                        <img src={GitHubIcon} alt="GitHub" className="rounded-full w-8 h-8 hover:bg-blue-500" />
+                    </a>
+                    <a href="mailto:singodiyashubham87@gmail.com" className="hover:bg-blue-300 p-1 rounded-full">
+                        <img src={EmailIcon} alt="Email" className="rounded-full w-8 h-8 hover:bg-blue-500" />
+                    </a>
+                </div>
             </div>
-            <div className="footerBottom">
-                <p>Copyright &copy;2024; Designed by <span className="designer">Shubham Singodiya</span></p>
+            <div className="text-center mt-4">
+                <p className="text-white">Copyright &copy;2024; Designed by <span className="font-bold">Shubham Singodiya</span></p>
             </div>
         </footer>
     );
