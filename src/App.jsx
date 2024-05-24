416a89970c2b652cd50fb27a16972739c11c590b
Karan-Semwal <kar9205an@gmail.com>
2024-05-24 16:31:01 +0530

    Added responsiveness to Menu bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import { useEffect, useRef, useState } from "react";
 import { FaMoon, FaRegEye, FaRegEyeSlash, FaSun } from "react-icons/fa";
 import { RxCross1 } from "react-icons/rx";
 import Joyride from "react-joyride";
-import BgColor from "./components/BgColor";
 import Menu from "./components/Menu";
 import { handleUpdates, startDrawing } from "./utils/canvas";
-import { rainbowColors } from "./utils/helpers";
 import { SiBuymeacoffee } from "react-icons/si";
 import { tourSteps } from "./utils/helpers";
-
+import BgColorSidePanel from "./components/BgColorSidePanel";
 
 function App() {
   const canvasRef = useRef(null);
@@ -46,7 +44,7 @@ function App() {
 
   return (
     <>
-      <div className="bg-[#CBCCCF] flex flex-col min-w-full justify-center gsm:flex-row dark:bg-zinc-800 dark:bg-blend-luminosity dark:text-white transform transition duration-500 ease-in-out font-primary">
+      <div className="bg-[#CBCCCF] min-h-screen flex justify-center gsm:flex-row dark:bg-zinc-800 dark:bg-blend-luminosity dark:text-white transform transition duration-500 ease-in-out font-primary">
         <Joyride
           steps={steps}
           continuous
@@ -60,47 +58,12 @@ function App() {
           }}
         />
 
-        <div className=" flex flex-col min-w-full justify-center gsm:flex-row">
-          {showMenuAndBgColor && (
-            <div className="color-pallet gsm:w-[10%] w-[85%] h-[50rem] p-7 grid grid-cols-6 vsm:grid-cols-4 gsm:grid-cols-1 gap-2 vsm:gap-4 gsm:gap-2 gsm:pb-[5rem] gsm:pt-[14rem] gsm:mb-8 mx-auto">
-              <input
-                type="color"
-                name="color"
-                id="color"
-                title="Color Picker"
-                // defaultValue={bgColor}
-                onChange={(e) => setBgColor(e.target.value)}
-                className={`cursor-pointer m-auto w-[2rem] h-[2rem] vsm:w-[3rem] vsm:h-[3rem]  rounded-[0.4rem] border-[0.2px] border-black bg-gradient-to-r from-red-700 via-yellow-600 to-green-600 `}
-              />
-              {rainbowColors?.map((val, i) => (
-                <BgColor
-                  key={i}
-                  color={val}
-                  setBgColor={setBgColor}
-                  canvas={canvasRef.current}
-                />
-              ))}
-            </div>
-          )}
+        <div className="flex flex-col min-w-full justify-center gsm:flex-row">
+          {showMenuAndBgColor && <BgColorSidePanel canvasRef={canvasRef} setBgColor={setBgColor} />}
 
-          <div className="container w-[90%] gsm:min-h-[100dvh] flex flex-col justify-center items-center gap-[2rem] font-primary m-auto gsm:m-0">
-            {/* Buy me a coffee element */}
-            <a
-              href={BUY_ME_COFFEE_LINK}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex justify-end mt-4 w-[90%] sm:ml-0"
-            >
-              <button className="flex items-center bg-transparent border border-black text-black focus:outline-none bg-[#d4d5d7] hover:bg-[#c6c9ce] rounded-xl p-2">
-                <SiBuymeacoffee className="text-xl mx-auto sm:mr-2" />{" "}
-                {/* Icon */}
-                <span className="hidden sm:block text-base font-cursive">
-                  Buy me a Coffee
-                </span>{" "}
-                {/* Text */}
-              </button>
-            </a>
-            <div className="flex items-center gap-14">
+          <div className="flex flex-col justify-between mt-[2vh] items-center font-primary">
+            {/* Drawing Toolbar */}
+            <div className="flex items-center">
               {showMenuAndBgColor && (
                 <Menu
                   isDrawing={isDrawing}
@@ -117,11 +80,13 @@ function App() {
                   setSelectedTool={setSelectedTool}
                 />
               )}
-
-              <div className="flex flex-row justify-center align-center space-x-10">
+              {/* Toolbar right menu section */}
+              <div className="flex md:flex-row flex-col justify-center align-center items-center absolute top-[2vh] right-2">
+                {/* Eye button */}
                 <div
-                  className={`clearAll bg-[#CBCCCF] p-[1rem] text-[1.5rem] rounded-[50%] shadow-black shadow-md  transform transition duration-300 ease-in-out text-black hover:bg-gray-400 cursor-pointer dark:bg-slate-800 dark:text-[#ffffff] hover:md:scale-110 ${!showMenuAndBgColor &&
-                    "mt-10"}`}
+                  className={`bg-[#CBCCCF] scale-[0.7] p-[1rem] text-[1.5rem] w-80% rounded-[50%] shadow-black shadow-md transform transition duration-300 ease-in-out text-black hover:bg-gray-400 cursor-pointer dark:bg-slate-800 dark:text-[#ffffff] hover:md:scale-[0.8] ${
+                    !showMenuAndBgColor && "mt-10"
+                  }`}
                   onClick={() => {
                     setShowMenuAndBgColor((state) => !state);
                   }}
@@ -129,31 +94,42 @@ function App() {
                   {showMenuAndBgColor ? <FaRegEyeSlash /> : <FaRegEye />}
                 </div>
 
+                {/* Theme Changer */}
                 <div
-                  className={`darkLightModeToggle  p-[1rem] text-[1.5rem] rounded-[50%] shadow-md hover:bg-gray-1000 transform transition duration-300 ease-in-out hover:md:scale-110 cursor-pointer bg-black dark:bg-amber-400 shadow-black dark:shadow-black dark:shadow-md ${!showMenuAndBgColor &&
-                    "mt-10"}`}
+                  className={`p-[1rem] text-[1.5rem] scale-[0.7] rounded-[50%] shadow-md hover:bg-gray-1000 transform transition duration-300 ease-in-out hover:md:scale-[0.8] cursor-pointer bg-black dark:bg-amber-400 shadow-black dark:shadow-black dark:shadow-md ${
+                    !showMenuAndBgColor && "mt-10"
+                  }`}
                   onClick={toggleDarkMode}
                 >
-                  {darkMode ? (
-                    <FaSun className="text-black" />
-                  ) : (
-                    <FaMoon className="text-white" />
-                  )}
+                  {darkMode ? <FaSun className="text-black" /> : <FaMoon className="text-white" />}
                 </div>
+
+                {/* Buy me a coffee */}
+                <a
+                  href={BUY_ME_COFFEE_LINK}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex justify-end sm:ml-0"
+                >
+                  <button className="flex mt-2 mb-2 items-center ml-1 mr-1 bg-transparent border border-black text-black focus:outline-none bg-[#d4d5d7] hover:bg-[#c6c9ce] rounded-xl p-2">
+                    <SiBuymeacoffee className="text-xl" />
+                  </button>
+                </a>
               </div>
             </div>
+
+            {/* ----- Canvas ------ */}
             <canvas
               id="draw"
-              className={`whiteboard bg-slate-950 rounded-[0.6rem] shadow-md shadow-black dark:shadow-black dark:shadow-lg ${
-                isDrawing
-                  ? "cursor-crosshair"
-                  : "cursor-default pointer-events-none"
+              className={`whiteboard bg-slate-950 w-screen mt-[4vh] rounded-[0.6rem] shadow-md shadow-black dark:shadow-black dark:shadow-lg ${
+                isDrawing ? "cursor-crosshair" : "cursor-default pointer-events-none"
               }
             `}
               ref={canvasRef}
             ></canvas>
+
             <div
-              className="clearAll bg-[#CBCCCF] p-[1rem] text-[2rem] rounded-[50%] shadow-black shadow-vsm dark:shadow-black dark:shadow-lg hover:bg-gray-400 cursor-pointer transform transition duration-300 ease-in-out dark:bg-red-700 dark:text-[#111111]  hover:md:scale-110"
+              className="bg-[#CBCCCF] p-[1rem] text-[2rem] rounded-[50%] shadow-black shadow-vsm dark:shadow-black dark:shadow-lg hover:bg-gray-400 cursor-pointer transform transition duration-300 ease-in-out dark:text-[#111111]  hover:md:scale-110"
               onClick={() => {
                 setBgColor("#B7BABF");
                 setBrushStyle("solid");
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,10 +2,10 @@
 import { PiPencilSimpleFill } from "react-icons/pi";
 import { FaChevronDown } from "react-icons/fa";
 import { FaFeatherPointed } from "react-icons/fa6";
-import { RiScreenshot2Fill } from "react-icons/ri";
 import { FaFilePdf } from "react-icons/fa";
 import { TbFileTypeSvg } from "react-icons/tb";
 import { useState } from "react";
+import { IoCloudDownloadOutline } from "react-icons/io5";
 
 import {
   convertToPDF,
@@ -20,43 +20,10 @@ import { decreaseHeight } from "../utils/canvas.js";
 import DrawingShapes from "./DrawingShapes.jsx";
 // import BgColor from "./BgColor.jsx";
 
-const Menu = ({
-  thickness,
-  setThickness,
-  color,
-  setColor,
-  canvasRef,
-  setBrushStyle,
-  bgColor,
-  brushStyle,
-  selectedTool,
-  setSelectedTool,
-}) => {
-  const [pencilWidth, setPencilWidth] = useState(false);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
-  const [fillColor, setFillColor] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
-  const handleMouseEnter = () => {
-    setIsOpen(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsOpen(false);
-  };
-
-  const handleBrushStyleChange = (style) => {
-    setBrushStyle(style);
-    setIsDropdownOpen(false); // Close the dropdown after selecting a style
-  };
+function Brush(props) {
+  const { isDropdownOpen, toggleDropdown, setBrushStyle, setIsDropdownOpen, brushStyle } = props;
   return (
-    <>
-      <div className="max-w-[90%] flex-wrap	 tools bg-[#CBCCCF] shadow-mdm dark:bg-[#111111] shadow-black flex justify-center   items-center gap-[1rem] md:gap-[3rem] px-[2rem] pt-6 pb-10 rounded-[0.6rem]  ">
-        <div className="relative ">
+    <div className="relative ">
           <PiPencilSimpleFill
             className={`text-[2rem] md:text-[3rem] p-[0.5rem] md:p-[0.8rem] shadow-vsm rounded-[0.5rem] cursor-pointer text-black bg-[#CBCCCF] hover:bg-[#B7BABF] ${
               isDropdownOpen ? "bg-gray-400" : ""
@@ -65,7 +32,7 @@ const Menu = ({
             title="Draw"
           />
           <div
-            className={`absolute top-full bg-[#CBCCCF] shadow-black  mx-auto rounded-[0.5rem] left-1/2 transform -translate-x-1/2  shadow-md ${
+            className={`absolute top-full bg-[#CBCCCF]   mx-auto rounded-[0.5rem] left-1/2 transform -translate-x-1/2 ${
               isDropdownOpen ? "block" : "hidden"
             }`}
           >
@@ -124,6 +91,53 @@ const Menu = ({
             }`}
           />
         </div>
+  )
+}
+
+const Menu = ({
+  thickness,
+  setThickness,
+  color,
+  setColor,
+  canvasRef,
+  setBrushStyle,
+  bgColor,
+  brushStyle,
+  selectedTool,
+  setSelectedTool,
+}) => {
+  const [pencilWidth, setPencilWidth] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [fillColor, setFillColor] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleMouseEnter = () => {
+    setIsOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsOpen(false);
+  };
+
+  const handleBrushStyleChange = (style) => {
+    setBrushStyle(style);
+    setIsDropdownOpen(false); // Close the dropdown after selecting a style
+  };
+
+  return (
+    <>
+      <div className="scale-[0.8] max-w-[100%] bg-[#CBCCCF] shadow-mdm dark:bg-[#111111]  flex justify-center items-center gap-[1rem] px-[1rem] pt-2 pb-2 rounded-[0.6rem]">
+        <Brush
+          isDropdownOpen={isDropdownOpen}
+          toggleDropdown={toggleDropdown}
+          setBrushStyle={setBrushStyle}
+          setIsDropdownOpen={setIsDropdownOpen}
+          brushStyle={brushStyle}
+        />
         <DrawingShapes
           brushWidth={thickness}
           selectedColor={color}
@@ -132,41 +146,42 @@ const Menu = ({
           selectedTool={selectedTool}
           setSelectedTool={setSelectedTool}
         />
-        <button>
+        <button className="flex flex-col items-center gap-1 mt-2">
           <input
             type="checkbox"
             id="fill-color"
             onChange={(e) => setFillColor(e.target.checked)}
             title="Fill Color"
           />
-          <label htmlFor="fill-color">Fill color</label>
+          <label htmlFor="fill-color">Fill</label>
         </button>
-        <button className="relative">
-          <FaFeatherPointed
-            className={`text-[2rem] md:text-[3rem] p-[0.5rem] md:p-[0.8rem] shadow-black shadow-vsm mx-auto rounded-[0.5rem] text-black  bg-[#CBCCCF] cursor-pointer hover:bg-[#B7BABF]transform transition duration-300 ease-in-out ${
-              pencilWidth ? "bg-gray-200" : ""
-            }`}
-            onClick={() => setPencilWidth(!pencilWidth)}
-            title="Brush Thickness"
-          />
-          <span className=" absolute -left-3 top-14">Thickness</span>
-        </button>
-
-        {pencilWidth && (
-          <input
-            type="range"
-            name="thickness"
-            id="thickness"
-            value={thickness || 10}
-            min={1}
-            max={100}
-            onChange={(e) => {
-              setThickness(e.target.value);
-            }}
-            className="cursor-pointer"
-          />
-        )}
-        <div className="py-[1rem] px-[1.5rem] rounded-[0.5rem] w-12 h-12 relative shadow-black shadow-vsm  hover:bg-[#B7BABF] text-black text-blackcursor-pointer">
+        
+        <div className="flex flex-col relative">
+          <button className="relative">
+            <FaFeatherPointed
+              className={`text-[2rem] md:text-[3rem] p-[0.5rem] md:p-[0.8rem] shadow-vsm mx-auto rounded-[0.5rem] text-black bg-[#CBCCCF] cursor-pointer hover:bg-[#B7BABF]transform transition duration-300 ease-in-out ${
+                pencilWidth ? "bg-gray-200" : ""
+              }`}
+              onClick={() => setPencilWidth(!pencilWidth)}
+            />
+            {/* <span className=" absolute -left-3 top-14">Thickness</span> */}
+          </button>
+          {pencilWidth && (
+            <input
+              type="range"
+              name="thickness"
+              id="thickness"
+              value={thickness || 10}
+              min={1}
+              max={100}
+              onChange={(e) => {
+                setThickness(e.target.value);
+              }}
+              className="cursor-pointer absolute bottom-[-40px]"
+            />
+          )}
+        </div>
+        <div className="py-[1rem] px-[1.5rem] rounded-[0.5rem] w-12 h-12 relative  shadow-vsm  hover:bg-[#B7BABF] text-black text-blackcursor-pointer">
           <input
             type="color"
             name="color"
@@ -176,7 +191,7 @@ const Menu = ({
             className={`bg-[#CBCCCF] p-[0.5rem] shadow-vsm rounded-[0.5rem] cursor-pointer outline-none hover:bg-[#B7BABF] flex-[0.5] w-full h-full z-[5] absolute top-0 left-0  transform transition duration-300 ease-in-out `}
           />
           <span className="absolute top-14 left-[0.3rem] dark:text-[#ffffff]">
-            Color
+            {/* Color */}
           </span>
         </div>
         <div className="relative">
@@ -189,7 +204,8 @@ const Menu = ({
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
           >
-            Save As
+            {/* Save As */}
+            <IoCloudDownloadOutline/>
             <svg
               className="w-2.5 h-2.5 ms-3"
               aria-hidden="true"
