01d6066c7c57f6450e4e09a054bbde37deace7b4
diyashah28 <di2004ya@gmail.com>
2024-06-06 11:13:50 +0530

    Background color change of canvas solved

--- src/components/BgColorSidePanel.jsx
+++ src/components/BgColorSidePanel.jsx
@@ -3,6 +3,11 @@ import { changeBG } from "../utils/canvas";
 import { rainbowColors } from "../utils/helpers";
 
 const BgColorSidePanel = ({ canvasRef, setBgColor }) => {
+  const handleColorChange = (e) => {
+    const color = e.target.value;
+    setBgColor(color);
+    changeBG(canvasRef.current, color);
+  }
   return (
     <div className="color-pallet gsm:w-[10%] w-[85%] h-[50rem] p-7 grid grid-cols-6 vsm:grid-cols-4 gsm:grid-cols-1 gap-2 vsm:gap-4 gsm:gap-2 gsm:pb-[5rem] gsm:pt-[14rem] gsm:mb-8 mx-auto">
       <input
@@ -11,7 +16,7 @@ const BgColorSidePanel = ({ canvasRef, setBgColor }) => {
         id="color"
         title="Color Picker"
         // defaultValue={bgColor}
-        onChange={(e) => setBgColor(e.target.value)}
+        onChange={handleColorChange}
         className={`cursor-pointer m-auto w-[2rem] h-[2rem] vsm:w-[3rem] vsm:h-[3rem]  rounded-[0.4rem] border-[0.2px] border-black bg-gradient-to-r from-red-700 via-yellow-600 to-green-600 `}
       />
       {rainbowColors?.map((val, i) => (
