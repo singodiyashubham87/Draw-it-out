abc6388bc6f0580a8de1eef59554fd83fc6981da
Sahil Thakur <sahilthakur2002@gmail.com>
2024-06-15 10:59:52 +0530

    Update main.jsx
    removed the style from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Preloader />
     <Chatbot />{ /*Render the Chatbot component*/ }
-    <App />
   </React.StrictMode>,
  document.getElementById('root')
 );
-
-document.body.style.userSelect = 'none';
-document.body.style.webkitUserSelect = 'none';
-document.body.style.mozUserSelect = 'none';
-document.body.style.msUserSelect = 'none';
