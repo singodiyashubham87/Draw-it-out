c35c880043aa5cbb01d5685f9996033e142f9380
Bhumika Bhatt <115869626+Bhumika1312@users.noreply.github.com>
2024-06-19 13:15:50 +0530

    Added a download button to upload the drawing in Google Drive

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {
+      VITE_CLIENT_ID: JSON.stringify(process.env.VITE_CLIENT_ID),
+      VITE_API_KEY: JSON.stringify(process.env.VITE_API_KEY),
+    },
+  },
 })
