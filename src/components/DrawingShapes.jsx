4bd134a175d086fd72c3af925b585a5c39dd2522
Ayush Deb <ayushdeb848@gmail.com>
2024-07-28 12:07:31 +0530

    fixed brush selector bug

--- src/components/DrawingShapes.jsx
+++ src/components/DrawingShapes.jsx
@@ -210,7 +210,8 @@ const DrawingShapes = ({
     <>
       <div>
         <button
-          className=" h-10 p-2 rounded-md text-xl shadow-md"
+          className=" h-10 p-2 rounded-md text-xl shadow-md hover:bg-gray-400"
+          style={{ backgroundColor: '#D1D5DB', color: '#000' }}
           onClick={() => setLower(!lower)}
         >
           <GiPencilBrush />
